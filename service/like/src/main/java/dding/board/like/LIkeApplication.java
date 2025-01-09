8f5964cc2b58db1048d28d976ed70aa9e21940e7
DDINGJOO <ddingsha9@teambind.co.kr>
2025-01-09 22:52:37 +0900

    Feat : Init_Project Date : 2025-01-09

---
+++ service/like/src/main/java/dding/board/like/LIkeApplication.java
@@ -0,0 +1,12 @@
+package dding.board.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LIkeApplication {
+    public static void  main(String[] args)
+    {
+        SpringApplication.run(LIkeApplication.class, args);
+    }
+}
