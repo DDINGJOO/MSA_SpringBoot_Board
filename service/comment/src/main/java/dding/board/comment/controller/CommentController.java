1ab5c3b2a41242c921f1e8c306f6dc027f048be7
DDINGJOO <ddingsha9@teambind.co.kr>
2025-01-14 16:50:43 +0900

    FEAT : COMMENT Controller Date : 2025-01-14 Content :

--- service/comment/src/main/java/dding/board/comment/controller/CommentController.java
+++ service/comment/src/main/java/dding/board/comment/controller/CommentController.java
@@ -1,4 +1,36 @@
 package dding.board.comment.controller;
 
+
+import dding.board.comment.dto.request.CommentCreateRequest;
+import dding.board.comment.dto.response.CommentResponse;
+import dding.board.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
 public class CommentController {
+    private final CommentService commentService;
+
+
+    @GetMapping("/v1/comments/{commentId}")
+    public CommentResponse read(
+            @PathVariable("commentId") Long commentId
+    )
+    {
+        return commentService.read(commentId);
+    }
+
+    @PostMapping("/v1/comments")
+    public CommentResponse create(
+            @RequestBody CommentCreateRequest req)
+    {
+        return commentService.create(req);
+    }
+
+    @DeleteMapping("/v1/comments/{commentId}")
+    public void delete(@PathVariable("commentId") Long commentId)
+    {
+        commentService.delete(commentId);
+    }
 }
