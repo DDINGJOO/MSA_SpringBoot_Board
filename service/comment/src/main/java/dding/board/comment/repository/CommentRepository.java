8fb87cfbe9310055ebffd99c49884d3760ed4c7a
DDINGJOO <ddingsha9@teambind.co.kr>
2025-01-14 16:19:30 +0900

    FEAT : COMMENT REPOSITORY Date : 2025-01-14 Content :

--- service/comment/src/main/java/dding/board/comment/repository/CommentRepository.java
+++ service/comment/src/main/java/dding/board/comment/repository/CommentRepository.java
@@ -2,9 +2,27 @@ package dding.board.comment.repository;
 
 import dding.board.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository  extends JpaRepository<Comment,Long> {
+
+
+    @Query(
+            value = "select count(*) from (" +
+                    "select comment_id from comment " +
+                    "where article_id = :articleId and parent_comment_id = :parentCommentId "+
+                    "limit :limit" +
+                    ") t",
+            nativeQuery = true
+    )
+
+    Long countBy(
+            @Param("article_id") Long articleId,
+            @Param("parent_comment_id") Long parentCommentId,
+            @Param("limit") Long limit
+    );
 
 }
