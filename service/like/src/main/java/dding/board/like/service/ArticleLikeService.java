120036857e6c7c8edb0d4222e863608f2ae783aa
DDINGJOO <ddingsha9@teambind.co.kr>
2025-01-15 20:51:27 +0900

    FEAT : ArticleLikeCount Service Date : 2025-01-15 Content :

--- service/like/src/main/java/dding/board/like/service/ArticleLikeService.java
+++ service/like/src/main/java/dding/board/like/service/ArticleLikeService.java
@@ -2,6 +2,8 @@ package dding.board.like.service;
 
 import dding.board.like.dto.Response.ArticleLikeResponse;
 import dding.board.like.entity.ArticleLike;
+import dding.board.like.entity.ArticleLikeCount;
+import dding.board.like.repository.ArticleLikeCountRepository;
 import dding.board.like.repository.ArticleLikeRepository;
 import dding.board.like.util.PKProvider.SnowFlakePKProvider;
 import jakarta.transaction.Transactional;
@@ -14,6 +16,7 @@ public class ArticleLikeService {
 
     private final PKProvider pkProvider = new SnowFlakePKProvider();
     private final ArticleLikeRepository articleLikeRepository;
+    private  final ArticleLikeCountRepository articleLikeCountRepository;
 
 
     public ArticleLikeResponse read(Long articleId, Long userId)
@@ -42,5 +45,106 @@ public class ArticleLikeService {
         articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
                 .ifPresent(articleLikeRepository::delete);
     }
+
+
+    /**
+     * update 구문 날리기
+     */
+    @Transactional
+    public void likePessimisticLock1(Long articleId, Long userId)
+    {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        pkProvider.getId(),
+                        articleId,
+                        userId
+            )
+        );
+        int result = articleLikeCountRepository.increase(articleId);
+        if(result ==0)
+        {
+            //최초 요청시 -> 게시글 최초 생성시 그냥 0 으로 설정하는 방법이..?
+            articleLikeCountRepository.save(
+                    ArticleLikeCount.create(articleId,1L)
+
+            );
+        }
+
+
+    }
+
+    @Transactional
+    public void unLikePessimisticLock1(Long articleId, Long userId)
+    {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLike -> {
+                    articleLikeRepository.delete(articleLike);
+                    articleLikeCountRepository.decrease(articleId);
+                } );
+    }
+
+    /**
+     * select ... for update ...
+     */
+    @Transactional
+    public void likePessimisticLock2(Long articleId, Long userId)
+    {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        pkProvider.getId(),
+                        articleId,
+                        userId
+                )
+        );
+
+        ArticleLikeCount articleLikeCount = articleLikeCountRepository.findLockedByArticleId(articleId)
+                .orElseGet(()-> ArticleLikeCount.create(articleId,0L));
+        articleLikeCount.increase();
+        articleLikeCountRepository.save(articleLikeCount);
+    }
+
+
+    @Transactional
+    public void unlikePessimisticLock2(Long articleId, Long userId)
+    {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLike -> {
+                    articleLikeRepository.delete(articleLike);
+                    ArticleLikeCount articleLikeCount = articleLikeCountRepository.findLockedByArticleId(articleId).orElseThrow();
+                    articleLikeCount.decrease();;
+                } );
+
+    }
+
+    /**
+     * optimistic Lock
+     */
+    @Transactional
+    public void likeOptimisticLock(Long articleId, Long userId)
+    {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        pkProvider.getId(),
+                        articleId,
+                        userId
+                )
+        );
+        var articleLikeCount = articleLikeCountRepository.findById(articleId).orElseGet(() -> ArticleLikeCount.create(articleId, 0L));
+        articleLikeCount.increase();
+        articleLikeCountRepository.save(articleLikeCount);
+    }
+
+
+    @Transactional
+    public void unlikeOptimisticLock(Long articleId, Long userId)
+    {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLike -> {
+                    articleLikeRepository.delete(articleLike);
+                    var articleLikeCount = articleLikeCountRepository.findById(articleId).orElseThrow();
+                    articleLikeCount.decrease();
+                });
+    }
+
 
 }
