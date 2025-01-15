231f817153d665e7b1d6349d540e97741e22e0af
DDINGJOO <ddingsha9@teambind.co.kr>
2025-01-15 20:51:19 +0900

    FEAT : ArticleLikeCount Service Date : 2025-01-15 Content :

---
+++ service/like/src/main/java/dding/board/like/service/ArticleLikeCountService.java
@@ -0,0 +1,10 @@
+package dding.board.like.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleLikeCountService {
+}
