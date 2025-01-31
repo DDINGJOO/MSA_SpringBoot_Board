2606d3511bc60a2c797b7aca5cd9437a1a7b3473
DDINGJOO <ddingsha9@teambind.co.kr>
2025-01-31 21:28:06 +0900

    FEAT : Board_article_count Controller Date : 2025-01-31
    Content : Make Controller
    for Each Board's  Articles Count

---
+++ service/article/src/main/java/dding/board/article/entity/BoardArticleCount.java
@@ -0,0 +1,34 @@
+package dding.board.article.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "board_article_count")
+@ToString
+public class BoardArticleCount
+{
+    @Id
+    private Long boardId;  //shardKey
+
+    private Long articleCount;
+
+
+
+    public static BoardArticleCount init(Long boardId)
+    {
+        var boardArticleCount = new BoardArticleCount();
+        boardArticleCount.boardId = boardId;
+        boardArticleCount.articleCount = 1L;
+        return boardArticleCount;
+    }
+}
