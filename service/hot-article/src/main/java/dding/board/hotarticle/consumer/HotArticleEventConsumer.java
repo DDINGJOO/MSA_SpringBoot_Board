0da96b6063ef855035735380838f7001168b3e85
DDINGJOO <ddingsha9@teambind.co.kr>
2025-02-05 02:29:38 +0900

    FEAT : Hot-Article Consumer Date : 2025-02-05
    Content :

---
+++ service/hot-article/src/main/java/dding/board/hotarticle/consumer/HotArticleEventConsumer.java
@@ -0,0 +1,35 @@
+package dding.board.hotarticle.consumer;
+
+import dding.board.common.event.Event;
+import dding.board.common.event.EventPayload;
+import dding.board.common.event.EventType;
+import dding.board.hotarticle.service.HotArticleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HotArticleEventConsumer {
+    private final HotArticleService hotArticleService;
+
+    @KafkaListener(topics = {
+            EventType.Topic.DDING_BOARD_ARTICLE,
+            EventType.Topic.DDING_BOARD_COMMENT,
+            EventType.Topic.DDING_BOARD_LIKE,
+            EventType.Topic.DDING_BOARD_VIEW
+    })
+    public void listen(String message , Acknowledgment ack)
+    {
+        log.info("[HotArticleEventConsumer.listen] received message = {}", message);
+        Event<EventPayload> event = Event.fromjson(message);
+        if(event!= null)
+        {
+            hotArticleService.handleEvent(event);
+        }
+        ack.acknowledge();
+    }
+}
