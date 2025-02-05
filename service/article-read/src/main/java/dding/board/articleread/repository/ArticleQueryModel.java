1110af8e0746e6d3e7e0ccb75aa797cc8863ca97
DDINGJOO <ddingsha9@teambind.co.kr>
2025-02-06 03:41:55 +0900

    FEET : ARTICLE READ REPOSITORY Date : 2025-02-06
    Content :

---
+++ service/article-read/src/main/java/dding/board/articleread/repository/ArticleQueryModel.java
@@ -0,0 +1,80 @@
+package dding.board.articleread.repository;
+
+
+import dding.board.articleread.client.ArticleClient;
+import dding.board.common.event.payload.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ArticleQueryModel {
+    private Long articleId;
+    private String title;
+    private String content;
+    private Long boardId;
+    private Long writerId;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private Long articleCommentCount;
+    private Long articleLikeCount;
+
+
+    public static ArticleQueryModel create(ArticleCreatedEventPayload payload)
+    {
+        ArticleQueryModel articleQueryModel = new ArticleQueryModel();
+        articleQueryModel.articleCommentCount = 0L;
+        articleQueryModel.articleLikeCount = 0L;
+        articleQueryModel.createdAt = payload.getCreatedAt();
+        articleQueryModel.writerId = payload.getWriterId();
+        articleQueryModel.articleId = payload.getArticleId();
+        articleQueryModel.boardId =payload.getBoardId();
+        articleQueryModel.modifiedAt = payload.getModifiedAt();
+        articleQueryModel.content = payload.getContent();
+        articleQueryModel.title = payload.getTitle();
+        return articleQueryModel;
+    }
+
+    public static ArticleQueryModel create(ArticleClient.ArticleResponse article, Long commentCount, Long likeCount)
+    {
+        ArticleQueryModel articleQueryModel = new ArticleQueryModel();
+        articleQueryModel.articleCommentCount = commentCount;
+        articleQueryModel.articleLikeCount = likeCount;
+        articleQueryModel.createdAt = article.getCreatedAt();
+        articleQueryModel.writerId = article.getWriterId();
+        articleQueryModel.articleId = article.getArticleId();
+        articleQueryModel.boardId =article.getBoardId();
+        articleQueryModel.modifiedAt = article.getModifiedAt();
+        articleQueryModel.content = article.getContent();
+        articleQueryModel.title = article.getTitle();
+        return articleQueryModel;
+    }
+
+    public void updateBy(CommentCreatedEventPayload payload)
+    {
+        this.articleCommentCount = payload.getArticleCommentCount();
+    }
+    public void updateBy(CommentDeletedEventPayload payload)
+    {
+        this.articleCommentCount = payload.getArticleCommentCount();
+    }
+    public void updateBy(ArticleLikedEventPayload payload)
+    {
+        this.articleCommentCount = payload.getArticleLikeCount();
+    }
+    public void updateBy(ArticleUnlikedEventPayload payload)
+    {
+        this.articleCommentCount = payload.getArticleLikeCount();
+    }
+
+    public void updateBy(ArticleUpdatedEventPayload payload)
+    {
+        this.title =payload.getTitle();
+        this.boardId = payload.getBoardId();
+        this.content = payload.getContent();
+        this.writerId = payload.getWriterId();
+        this.modifiedAt = payload.getModifiedAt();
+        this.createdAt = payload.getCreatedAt();
+
+    }
+}
