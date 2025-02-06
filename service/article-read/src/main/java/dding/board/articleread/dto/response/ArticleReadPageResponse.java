7e9abe98dd087fe2de3bb7d912da1c65b8b5b5ba
DDINGJOO <ddingsha9@teambind.co.kr>
2025-02-06 13:11:30 +0900

    FEAT : ArticleReadPageResponse Date : 2025-02-06
    Content :

---
+++ service/article-read/src/main/java/dding/board/articleread/dto/response/ArticleReadPageResponse.java
@@ -0,0 +1,23 @@
+package dding.board.articleread.dto.response;
+
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+
+public class ArticleReadPageResponse {
+    private List<ArticleReadResponse> articles;
+    private Long articleCount;
+
+    public static ArticleReadPageResponse of(List<ArticleReadResponse> articles,Long articleCount)
+    {
+        ArticleReadPageResponse response = new ArticleReadPageResponse();
+        response.articles = articles;
+        response.articleCount = articleCount;
+        return response;
+    }
+}
