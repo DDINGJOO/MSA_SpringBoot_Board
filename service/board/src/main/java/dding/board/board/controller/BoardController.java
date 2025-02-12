9c43f616ffe2475523cbf1937edb8a05d0978085
DDINGJOO <ddingsha9@teambind.co.kr>
2025-02-13 00:11:45 +0900

    FEAT : Board Controller Date : 2025-02-13
    Content :

--- service/board/src/main/java/dding/board/board/controller/BoardController.java
+++ service/board/src/main/java/dding/board/board/controller/BoardController.java
@@ -1,11 +1,37 @@
 package dding.board.board.controller;
 
+import dding.board.board.dto.request.BoardCreateRequest;
+import dding.board.board.dto.response.BoardResponse;
 import dding.board.board.entity.Board;
-import org.springframework.web.bind.annotation.RestController;
+import dding.board.board.service.BoardService;
+import jakarta.transaction.Transactional;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class BoardController {
+
+    BoardService boardService;
+
+    @PostMapping("/v1/boards")
+    @Transactional
+    public BoardResponse create (@RequestBody BoardCreateRequest req)
+    {
+        return boardService.create(req);
+    }
+
+    @GetMapping("/v1/boards/{boardId}")
+    public BoardResponse read(@PathVariable("boardId") Long boardId )
+    {
+        return boardService.read(boardId);
+    }
+
+    @DeleteMapping("/v1/boards/{boardId")
+    public void delete(@PathParam("boardId") Long boardId)
+    {
+        boardService.delete(boardId);
+    }
 
 
 
